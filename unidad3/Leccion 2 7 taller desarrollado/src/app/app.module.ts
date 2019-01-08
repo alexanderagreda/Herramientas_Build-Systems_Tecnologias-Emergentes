6abbbb38c0a4f21ec90bd6a4c97de28d6e47cdbb
alexander <alexanderagreda@gmail.com>
2019-01-08 18:06:00 -0500

    taller components

---
+++ unidad3/Leccion 2 7 taller desarrollado/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarraSuperiorComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
